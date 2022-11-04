79d50478bc1ab8e7caf02cf7df8d07cd803ce1d8
vamshikrishna26 <117000942+vamshikrishna26@users.noreply.github.com>
2022-11-04 16:03:56 +0530

    Add files via upload

---
+++ largenumberamongthreeusingifstmts.py
@@ -0,0 +1,12 @@
+a=int(input("enter a value  : "))
+b=int(input("enter b value  : "))
+c=int(input("enter c value  : "))
+
+if a>b and a>c:
+    print("a is greater number")
+
+if b>a and b>c:
+    print("b is greater number")
+
+if c>a and c>b:
+    print("c is greater number")
